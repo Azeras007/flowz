40cdb9c0b89680fd5c111734bb35426003190fa5
Laurans Antoine <antoinelaurans@epitech.eu>
2024-10-08 11:46:24 +0200

    Refactor DashboardView to use NavigationView and update UI layout

--- client_mobile/area-development/DashboardView.swift
+++ client_mobile/area-development/DashboardView.swift
@@ -7,77 +7,98 @@ struct DashboardView: View {
     @State private var recommendedApps: [String] = []
 
     var body: some View {
-        ZStack {
-            VStack {
-                Text("Dashboard")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.primary)
-                    .padding(.top, 20)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("Dashboard")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.primary)
+                        .padding(.top, 20)
 
-                Spacer().frame(height: 20)
+                    NavigationLink(
+                        destination: AreaListView()
+                    ) {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.yellow)
+                            .shadow(radius: 2)
+                            .frame(height: 70)
+                            .overlay(
+                                Text("Voir les Areas")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            )
+                            .padding(.top, 20)
+                            .padding(.horizontal, 20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Spacer().frame(height: 20)
 
-                VStack(alignment: .leading) {
-                    RoundedRectangle(cornerRadius: 12)
-                        .strokeBorder(Color(hue: 20 / 360, saturation: 5.9 / 100, brightness: 90 / 100), lineWidth: 2)
-                        .background(Color.clear)
-                        .shadow(radius: 2)
-                        .frame(height: 200)
-                        .overlay(
-                            VStack(alignment: .leading, spacing: 0) {
-                                Text("Activité récentes :")
-                                    .font(.headline)
-                                    .foregroundColor(Color.primary)
-                                    .padding(.top, 30)
-                                    .padding(.leading, 30)
+                    VStack(alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 12)
+                            .strokeBorder(Color(hue: 20 / 360, saturation: 5.9 / 100, brightness: 90 / 100), lineWidth: 2)
+                            .background(Color.clear)
+                            .shadow(radius: 2)
+                            .frame(height: 200)
+                            .overlay(
+                                VStack(alignment: .leading, spacing: 0) {
+                                    Text("Activité récentes :")
+                                        .font(.headline)
+                                        .foregroundColor(Color.primary)
+                                        .padding(.top, 30)
+                                        .padding(.leading, 30)
 
-                                Spacer()
-
-                                HStack {
                                     Spacer()
-                                    Button(action: {
-                                    }) {
-                                        Image(systemName: "arrow.forward.square.fill")
-                                            .resizable()
-                                            .frame(width: 35, height: 35)
-                                            .foregroundColor(Color.yellow)
+
+                                    HStack {
+                                        Spacer()
+                                        Button(action: {
+                                        }) {
+                                            Image(systemName: "arrow.forward.square.fill")
+                                                .resizable()
+                                                .frame(width: 35, height: 35)
+                                                .foregroundColor(Color.yellow)
+                                        }
+                                        .padding(.trailing, 20)
+                                        .padding(.bottom, 20)
                                     }
-                                    .padding(.trailing, 20)
-                                    .padding(.bottom, 20)
+                                },
+                                alignment: .topLeading
+                            )
+                            .padding(.top, 10)
+
+                        RoundedRectangle(cornerRadius: 12)
+                            .strokeBorder(Color(hue: 20 / 360, saturation: 5.9 / 100, brightness: 90 / 100), lineWidth: 1)
+                            .background(Color.clear)
+                            .shadow(radius: 2)
+                            .frame(height: 200)
+                            .overlay(
+                                VStack(alignment: .leading, spacing: 0) {
+                                    Text("Notifications :")
+                                        .font(.headline)
+                                        .foregroundColor(Color.primary)
+                                        .padding(.top, 30)
+                                        .padding(.leading, 30)
+                                    Spacer()
                                 }
-                            },
-                            alignment: .topLeading
-                        )
-                        .padding(.top, 10)
+                                .allowsHitTesting(false),
+                                alignment: .topLeading
+                            )
+                            .padding(.top, 10)
+                    }
+                    .padding()
 
-                    RoundedRectangle(cornerRadius: 12)
-                        .strokeBorder(Color(hue: 20 / 360, saturation: 5.9 / 100, brightness: 90 / 100), lineWidth: 1)
-                        .background(Color.clear)
-                        .shadow(radius: 2)
-                        .frame(height: 200)
-                        .overlay(
-                            VStack(alignment: .leading, spacing: 0) {
-                                Text("Notifications :")
-                                    .font(.headline)
-                                    .foregroundColor(Color.primary)
-                                    .padding(.top, 30)
-                                    .padding(.leading, 30)
-                                Spacer()
-                            },
-                            alignment: .topLeading
-                        )
-                        .padding(.top, 10)
+                    Spacer()
                 }
-                .padding()
+                .background(Color(UIColor.systemBackground).edgesIgnoringSafeArea(.all))
+                
+                
 
-                Spacer()
-            }
-            .background(Color(UIColor.systemBackground).edgesIgnoringSafeArea(.all))
+                VStack {
+                    Spacer()
 
-            VStack {
-                Spacer()
-
-                CustomNavBar(selectedTab: $selectedTab, isPresentingCreateView: $isPresentingCreateView)
+                    CustomNavBar(selectedTab: $selectedTab, isPresentingCreateView: $isPresentingCreateView)
+                }
             }
         }
     }
